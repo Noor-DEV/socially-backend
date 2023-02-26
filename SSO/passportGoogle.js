a8cdb556d3fd095d95ee17cbd322aca53c8533b9
Noor-DEV <devnoor00@gmail.com>
2023-02-26 15:16:18 +0300

    maincallbackerrorrURL

--- SSO/passportGoogle.js
+++ SSO/passportGoogle.js
@@ -44,7 +44,7 @@ const passportGoogleStrategy = new Strategy(
   {
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/auth/google/callback",
+    callbackURL: process.env.MAIN_CALLBACK_URL,
   },
   verifyCb
 );
